2eaf0fdb1ccbe9d7f4208b5a8788502587810b79
Ren Huang <yeerhh@gmail.com>
2025-03-03 16:00:01 +0800

    bugfix

--- app/_components/menubar.js
+++ app/_components/menubar.js
@@ -58,7 +58,7 @@ export default function Menubar() {
                 data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent"
-                aria-expanded="false"
+                aria-expanded="true"
                 aria-label="Toggle navigation"
               >
                 <span className="navbar-toggler-icon"></span>
