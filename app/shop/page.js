8724be3e278e1d1f60e09937674be18473a43b95
Cindy <s10803950713@gmail.com>
2025-03-06 14:55:08 +0800

    shop-調整卡片、新增商品類別資料(測試用)

--- app/shop/page.js
+++ app/shop/page.js
@@ -2,20 +2,45 @@
 'use client'
 
 import Image from 'next/image'
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import styles from './shop.module.css'
-// import globalStyles from '../page.module.css'
 import Card from './_components/card'
-import Products from './_components/data.json'
+import Products from './_data/data.json'
+import Category from './_data/category.json'
 import { FaAngleLeft,FaAngleRight } from "react-icons/fa6";
 import IconLine from './_components/icon_line'
-import Link from 'next/link'
+// import Link from 'next/link'
+import { FaRegHeart,FaHeart } from "react-icons/fa";
 
 
 
 export default function PetsPage() {
   console.log(Products);
+  // 擴充一個能代表是否有加入收藏(我的最愛)的屬性fav，它是一個布林值，預設為false
+  const initState = Products.map((v) => {
+    return { ...v, fav: false }
+  })
+  // 因為需要切換網頁上的加入收藏圖示
+  const [products, setproducts] = useState(initState)  
   
+  // 處理切換fav屬性(布林值)
+  const onToggleFav = (product_id) => {
+    // 利用map展開陣列
+    const nextProduct = products.map((v) => {
+      // 在成員(物件)中比對isbn為bookIsbn的成員
+      if (v.id == product_id) {
+        // 如果比對出isbn為bookIsbn的成員，展開物件後fav布林值"反相"(!v.fav)
+        return { ...v, fav: !v.fav }
+      } else {
+        // 否則直接返回物件
+        return v
+      }
+    })
+
+    // 步驟3: 設定到狀態中
+    setproducts(nextProduct)
+  }
+
   return (
     <>
       <div className={styles.banner}>
@@ -37,28 +62,33 @@ export default function PetsPage() {
             <div className={styles.contain_title}>
               <IconLine key={'8折優惠區'} title={'8折優惠區'}/>
             </div>
-            <div className={styles.group}>
-                <div className={styles.groupBody}>
-                  <div className={styles.angle}>
-                    <FaAngleLeft/>
+            <div className={styles.contain_body}>
+              <div className={styles.group}>
+                  <div className={styles.groupBody}>
+                    <button className={styles.angle}>
+                      <FaAngleLeft/>
+                    </button>
+                    <div className={styles.cardGroup}>
+                      {products.map((product) => {
+                        return(
+                          <Card
+                            key={product.id}
+                            image={product.image}
+                            title={product.title}
+                            text1= {`$${product.price}`}
+                            text1_del={`$${product.price}`}
+                            btn_text={product.fav ? <FaHeart/> : <FaRegHeart/>}
+                            btn_color='red'
+                            btn_onclick={() => {onToggleFav(product.id)}}
+                          />
+                        )
+                      })}
+                    </div>
+                    <button className={styles.angle}>
+                      <FaAngleRight/>
+                    </button>
                   </div>
-                  <div className={styles.cardGroup}>
-                    {Products.map((product) => {
-                      return(
-                        <Card
-                          key={product.id}
-                          image={product.image}
-                          title={product.title}
-                          description={product.description}
-                          price={product.price}
-                        />
-                      )
-                    })}
-                  </div>
-                  <div className={styles.angle}>
-                    <FaAngleRight/>
-                  </div>
-                </div>
+              </div>
             </div>
           </div>
 
@@ -69,58 +99,38 @@ export default function PetsPage() {
             </div>
             <div className={styles.contain_body}>
               {/* subTitle */}
-              <div className={styles.group}>
-                <div className={styles.groupTitle}>
-                  <p>subTitle</p>
-                </div>
-                <div className={styles.groupBody}>
-                  <div className={styles.angle}>
-                    <FaAngleLeft/>
+              {Category.filter((category) => category.parent_id == 1).map((category) => (
+                <div className={styles.group} key={category.id}>
+                  <div className={styles.groupTitle}>
+                    <p>{category.category_name}</p>
                   </div>
-                  <div className={styles.cardGroup}>
-                    {Products.map((product) => {
-                      return(
-                        <Card
-                          key={product.id}
-                          image={product.image}
-                          title={product.title}
-                          description={product.description}
-                          price={product.price}
-                        />
-                      )
-                    })}
-                  </div>
-                  <div className={styles.angle}>
-                    <FaAngleRight/>
-                  </div>
-                </div>
-              </div>
-              <div className={styles.group}>
-                <div className={styles.groupTitle}>
-                  <p>subTitle</p>
-                </div>
-                <div className={styles.groupBody}>
-                  <div className={styles.angle}>
-                    <FaAngleLeft/>
-                  </div>
-                  <div className={styles.cardGroup}>
-                    {Products.map((product) => {
-                      return(
-                        <Card
-                          key={product.id}
-                          image={product.image}
-                          title={product.title}
-                          description={product.description}
-                          price={product.price}
-                        />
-                      )
-                    })}
-                  </div>
-                  <div className={styles.angle}>
-                    <FaAngleRight/>
+                  <div className={styles.groupBody}>
+                    <button className={styles.angle}>
+                      <FaAngleLeft/>
+                    </button>
+                    <div className={styles.cardGroup}>
+                      {products.filter((product) => product.category_id == category.id).map((product) => {
+                        return(
+                          <Card
+                            key={product.id}
+                            image={product.image}
+                            title={product.title}
+                            text1= {`$${product.price}`}
+                            text1_del={`$${product.price}`}
+                            btn_text={product.fav ? <FaHeart/> : <FaRegHeart/>}
+                            btn_color='red'
+                            btn_onclick={() => {onToggleFav(product.id)}}
+                          />
+                        )
+                      })}
+                    </div>
+                    <button className={styles.angle}>
+                      <FaAngleRight/>
+                    </button>
                   </div>
                 </div>
-              </div>
+              ))}
+              
             </div>
           </div>
         </div>
