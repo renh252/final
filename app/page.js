82714c085cd6595cee477dba40b26c77008b16a9
Ren Huang <yeerhh@gmail.com>
2025-03-07 11:06:17 +0800

    清理首頁和pets頁面的舊Banner

--- app/page.js
+++ app/page.js
@@ -1,18 +1,7 @@
 'use client'
 
-import Image from 'next/image'
-import React, { useState, useEffect } from 'react'
-export default function Page(props) {
-  return (
-    <>
-      <Image
-        src="/images/Banner.jpg"
-        alt="圖片描述"
-        layout='responsive'
-        width={300}
-        height={200}
-      />
-      <div>頁面內容edit</div>
-    </>
-  )
+import React from 'react'
+
+export default function Page() {
+  return (<div>頁面內容edit</div>)
 }
