19ab69862511ca32bfd159154def1047f58d0e20
Sheng <sheng22102@gmail.com>
2025-03-09 23:26:54 +0800

    更新捐款頁

--- app/donate/_components/methodItem.js
+++ app/donate/_components/methodItem.js
@@ -3,16 +3,24 @@
 import Image from 'next/image'
 import styles from '../donate.module.css'
 
-export default function MethodItem({imgSrc, alt, title, description }) {
+export default function MethodItem({ imgSrc, alt, title, description }) {
   return (
     <>
       <div className={styles.method_item}>
         <div className={styles.icon}>
-          <Image src={imgSrc} alt={alt} width={150} height={150}></Image>
+          {imgSrc ? (
+            <Image src={imgSrc} alt={alt} width={150} height={150} />
+          ) : (
+            ''
+          )}
         </div>
         <div className={styles.content}>
           <h5>{title}</h5>
-          {description}
+          {Array.isArray(description) ? (
+            description.map((text, index) => <p key={index}>{text}</p>)
+          ) : (
+            <p>{description}</p>
+          )}
         </div>
       </div>
     </>
