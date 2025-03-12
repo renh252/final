c5f9beecc85af5dfecae378dce44a6a5ecd5f655
Sheng <sheng22102@gmail.com>
2025-03-13 01:26:31 +0800

    合併

--- app/member/register/page.js
+++ app/member/register/page.js
@@ -5,7 +5,7 @@ import React, { useState, useEffect } from 'react'
 export default function RegisterPage(props) {
   return (
     <>
-      <div>會員註冊</div>
+      <div>Register Page</div>
     </>
   )
 }
