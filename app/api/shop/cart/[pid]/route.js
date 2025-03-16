af78f3b925013f94812cf0caec1464ec78478005
Cindy <hebe90713@gmail.com>
2025-03-16 20:23:39 +0800

    新增刪除單一購物車商品功能

--- app/api/shop/cart/[pid]/route.js
+++ app/api/shop/cart/[pid]/route.js
@@ -26,3 +26,33 @@ export async function PUT(request, { params }) {
     return NextResponse.json({ success: false, message: '更新購物車時發生錯誤' }, { status: 500 });
   }
 }
+
+export async function DELETE(request, { params }) {
+  const { pid } = params;
+  try {
+    const connection = await pool.getConnection();
+
+    // 更新購物車商品數量
+    const [result] = await connection.execute(
+      'DELETE FROM shopping_cart WHERE id = ?',
+      [pid]
+    );
+
+    connection.release();
+
+    
+    if (result.affectedRows > 0) {
+      return NextResponse.json({ 
+        success: true, 
+        message: '商品已成功從購物車中刪除' });
+    } else {
+      return NextResponse.json({ 
+        success: false, message: '找不到指定的購物車項目' }, 
+        { status: 404 });
+    }
+
+  }catch (error) {
+    console.error('刪除購物車商品時發生錯誤：', error);
+    return NextResponse.json({ success: false, message: '刪除購物車時發生錯誤' }, { status: 500 });
+  }
+}
