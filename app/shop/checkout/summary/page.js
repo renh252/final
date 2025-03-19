aae16eab3d41acd05f650b75dc2e95ad646094f9
Cindy <s10803950713@gmail.com>
2025-03-19 12:05:49 +0800

    新增css

--- app/shop/checkout/summary/page.js
+++ app/shop/checkout/summary/page.js
@@ -1,13 +1,47 @@
 'use client'
 
 import React, { useState, useEffect } from 'react'
-// import { useCheckoutData } from '@/app/shop/_components/useCheckoutData'
+import Link from 'next/link';
+// styles
+import styles from './summary.module.css'
+import { BsPatchCheck } from "react-icons/bs";
+
+// components
+import {Breadcrumbs} from '@/app/_components/breadcrumbs'
+
+
 
 export default function SummaryPage(props) {
-  // const [checkoutData, setCheckoutData] = useCheckoutData();
   return (
-    <>
-      <div>訂單明細</div>
+    <> 
+      <Breadcrumbs
+        title='訂單明細'
+        items={[
+          { label: '購物車', href: '/shop/cart' },
+          { label: '訂單明細', href: '/shop/checkout/summary' },
+        ]}
+        />
+        <div className={styles.main}>
+          <div>
+            <BsPatchCheck />
+            <p>訂單成立</p>
+          </div>
+          <div>
+            <Link href=''>查看訂單</Link>
+            <Link href='/shop'>繼續逛逛</Link>
+          </div>
+          <div className={styles.container}>
+            <div className={styles.containTitle}>
+
+            </div>
+            <div className={styles.containBody}>
+
+            </div>
+            <div className={styles.containFooter}>
+              
+            </div>
+          </div>
+        </div>
     </>
   )
 }
