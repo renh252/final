c54bd8800466955f31bfb0bf4efa5c6609a58a6c
Ren Huang <yeerhh@gmail.com>
2025-03-20 17:49:01 +0800

    更新頁面

--- app/member/FavoritesList/Pets/page.js
+++ app/member/FavoritesList/Pets/page.js
@@ -1,11 +1,25 @@
 'use client'
 
 import React, { useState, useEffect } from 'react'
+import { useAuth } from '@/app/context/AuthContext'
+import { useRouter } from 'next/navigation'
 
-export default function PetLikePage(props) {
+export default function PetLikePage() {
+  const { user } = useAuth()
+  const router = useRouter()
+
+
+  if (!user) {
+    return null
+  }
+
   return (
     <>
       <div>寵物收藏</div>
+      <div>用戶資訊：</div>
+      <div>ID: {user.id}</div>
+      <div>名稱: {user.name}</div>
+      <div>Email: {user.email}</div>
     </>
   )
 }
