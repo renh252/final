4626859d1ffeb78e2ff1d7f087656119205f0374
Cindy <s10803950713@gmail.com>
2025-03-21 13:25:18 +0800

    刪除orders原本樣式

--- app/member/orders/page.js
+++ app/member/orders/page.js
@@ -1,7 +1,6 @@
 'use client'
 import styles from './orders.module.css'
 import Link from 'next/link'
-
 import React, { useState, useEffect } from 'react'
 // components
 import List from '../_components/list'
@@ -55,50 +54,10 @@ export default function OrdersPage() {
             <p>備註 :  {order.remark}</p>
             <p>日期 : {formatDate(order.created_at)}</p>
             <p>狀態 : <StatusBadge status={order.payment_status} /></p>
-            
           </div>
       )
-
       }
-
-      
-
     />
-    <div className={styles.top}>
-      <select name="" id="">
-        <option value="">訂單狀態</option>
-      </select>
-      <select name="" id="">
-        <option value="">日期</option>
-      </select>
-      <p>共{totalOrders}筆資料</p>
-    </div>
-      <List 
-        title=        
-        <div className={styles.containTitle}>
-          <div><FaList /></div>
-          <div>編號</div>
-          <div>訂單狀態</div>
-          <div>付款狀態</div>
-          <div>金額</div>
-          <div>備註</div>
-          <div>日期</div>
-        </div>
-        body=        
-        <div className={styles.containBody}>
-        {orders.map((order)=>{return (
-          <div className={styles.order} key={order.order_id }>
-            <div><Link href={`/member/orders/${order.order_id}`}><FaList /></Link></div>
-            <div>{order.order_id }</div>
-            <div>{order.order_status}</div>
-            <div>{order.payment_status}</div>
-            <div>${order.total_price}</div>
-            <div>{order.remark}</div>
-            <div>{formatDate(order.created_at)}</div>
-          </div>
-        )})}
-        </div>
-      />
     </>
   ) 
 }
