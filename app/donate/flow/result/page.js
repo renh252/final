878483b35fdafac1568ffd0e4f7f3c8b7157ad3f
sheng221 <sheng22102@gmail.com>
2025-03-31 15:32:04 +0800

    商城&捐款完成頁統一樣式

--- app/donate/flow/result/page.js
+++ app/donate/flow/result/page.js
@@ -5,23 +5,11 @@ import { useRouter } from 'next/navigation'
 import styles from '../flow.module.css'
 import Image from 'next/image'
 import Link from 'next/link'
+import { BsPatchCheckFill } from 'react-icons/bs'
 
 export default function ResultPage(props) {
-  const [countdown, setCountdown] = useState(5) // 初始倒數時間 5 秒
   const router = useRouter()
 
-  useEffect(() => {
-    if (countdown === 0) {
-      router.push('/member/donations') // 倒數結束後跳轉捐款紀錄頁
-      return
-    }
-
-    const timer = setTimeout(() => {
-      setCountdown(countdown - 1)
-    }, 1000) // 每秒更新一次
-
-    return () => clearTimeout(timer) // 清除計時器
-  }, [countdown, router])
   return (
     <>
       <div className={styles.flow_container}>
@@ -30,21 +18,25 @@ export default function ResultPage(props) {
       </div>
       <div className={styles.order_container}>
         <div className={styles.result}>
-          <Image
-            src="/images/donate/icon/result.png"
-            alt="success"
-            width={150}
-            height={150}
-          />
+          <BsPatchCheckFill className={styles.successIcon} />
           <div>
-            <h1>捐款成功</h1>
-            <p>
-              將於<span style={{ color: 'red' }}>{countdown}</span>秒後跳至
-              <Link href="/member/donations" style={{ color: 'blue' }}>
-                捐款紀錄頁面
-              </Link>
-            </p>
+            <h1 className={styles.title}>捐款成功</h1>
           </div>
+        </div>{' '}
+        {/* 按鈕區塊 */}
+        <div className={styles.buttonGroup}>
+          <button
+            className={styles.button}
+            onClick={() => (window.location.href = '/member/donations')}
+          >
+            查看捐款紀錄
+          </button>
+          <button
+            className={styles.button}
+            onClick={() => (window.location.href = '/donate')}
+          >
+            回到捐款頁
+          </button>
         </div>
       </div>
     </>
