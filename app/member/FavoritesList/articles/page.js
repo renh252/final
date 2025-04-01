aa0de74d4075f5800bde6cd7fa91e59e4a6dd306
Ren Huang <yeerhh@gmail.com>
2025-04-01 10:29:22 +0800

    微調menubar樣式

--- app/member/FavoritesList/articles/page.js
+++ app/member/FavoritesList/articles/page.js
@@ -1,8 +1,11 @@
 'use client'
 
 import React, { useState, useEffect } from 'react'
+import { usePageTitle } from '@/app/context/TitleContext'
 
 export default function ArticleLikePage(props) {
+  usePageTitle('收藏文章')
+
   return (
     <>
       <div>文章收藏</div>
